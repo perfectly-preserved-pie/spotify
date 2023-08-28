86bc492348f027ca33911ffa64e1c26176b3bd04
Sahib Bhai <lunarwolf01@gmail.com>
2023-08-28 15:55:38 -0700

    Create a Dash app, save datasets

---
+++ app.py
@@ -0,0 +1,93 @@
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import dash_ag_grid as dag
+import pandas as pd
+
+# Load the data
+df_artists = pd.read_parquet("datasets/top_artists.parquet")
+df_tracks = pd.read_parquet("datasets/top_tracks.parquet")
+
+# Function to create initial top artists grid
+def initial_top_artists_grid(time_range):
+    df = df_artists[df_artists["time_range"] == time_range]
+    return dag.AgGrid(
+        id="top-artists-ag-grid",
+        columnDefs=[
+            {"headerName": "Artist", "field": "name", "sortable": True, "filter": True},
+            {"headerName": "Genres", "field": "genres", "sortable": True, "filter": True},
+            {"headerName": "Time Range", "field": "time_range", "sortable": True, "filter": True},
+            {"headerName": "Images", "field": "images_small", "sortable": False, "filter": False, "cellRenderer": "imageCellRenderer"}
+        ],
+        rowData=df.to_dict("records"),
+        className="ag-theme-alpine-dark",
+    )
+
+# Function to create top tracks grid
+def create_top_tracks_grid(time_range):
+    df = df_tracks[df_tracks["time_range"] == time_range]
+    return dag.AgGrid(
+        id="top-tracks-ag-grid",
+        columnDefs=[
+            {"headerName": "Track", "field": "name", "sortable": True, "filter": True},
+            {"headerName": "Artist", "field": "artist", "sortable": True, "filter": True},
+            {"headerName": "Genres", "field": "genres", "sortable": True, "filter": True},
+            {"headerName": "Explicit", "field": "explicit", "sortable": True, "filter": True},
+            {"headerName": "Preview URL", "field": "preview_url", "sortable": False, "filter": False, "cellRenderer": "linkCellRenderer"},
+            {"headerName": "Images", "field": "images_small", "sortable": False, "filter": False, "cellRenderer": "imageCellRenderer"}
+        ],
+        rowData=df.to_dict("records"),
+        className="ag-theme-alpine-dark",
+    )
+
+# Initial default values
+default_time_range = "long_term"
+
+# Create the app
+external_stylesheets = [dbc.themes.DARKLY, dbc.icons.BOOTSTRAP, dbc.icons.FONT_AWESOME, "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.2/dbc.min.css"]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+# App layout
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            html.H1("Spotify Top Artists and Tracks", className="text-center"),
+            html.Hr(),
+            html.P("Select a time range to view your top artists and tracks for that time period.", className="text-center"),
+            dcc.Dropdown(
+                id="time-range-dropdown",
+                options=[
+                    {"label": "All Time", "value": "long_term"},
+                    {"label": "Last 6 Months", "value": "medium_term"},
+                    {"label": "Last 4 Weeks", "value": "short_term"}
+                ],
+                value=default_time_range,
+                clearable=False,
+                className="text-center ag-theme-alpine-dark"
+            ),
+            html.Hr(),
+            html.Div(id="top-artists-grid", children=initial_top_artists_grid(default_time_range)),
+            html.Hr(),
+            html.Div(id="top-tracks-grid", children=create_top_tracks_grid(default_time_range))
+        ], width=12)
+    ])
+])
+
+# Callbacks
+@app.callback(
+    Output("top-artists-grid", "children"),
+    Input("time-range-dropdown", "value")
+)
+def update_top_artists_grid(time_range):
+    return initial_top_artists_grid(time_range)
+
+@app.callback(
+    Output("top-tracks-grid", "children"),
+    Input("time-range-dropdown", "value")
+)
+def update_top_tracks_grid(time_range):
+    return create_top_tracks_grid(time_range)
+
+# Run the app
+if __name__ == "__main__":
+    app.run_server(debug=True)
