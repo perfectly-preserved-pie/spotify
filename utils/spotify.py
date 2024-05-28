b1a185c1a68023627100cbe6c8323a86606bfe70
Sahib Bhai <lunarwolf01@gmail.com>
2024-05-27 20:41:26 -0700

    Add type hints and docstrings to a bunch of functions. Also add a ranking to each row to keep track of movement over time

--- utils/spotify.py
+++ utils/spotify.py
@@ -1,137 +1,107 @@
 from dotenv import load_dotenv, find_dotenv
 from loguru import logger
-from spotipy.oauth2 import SpotifyOAuth
-from sqlalchemy import create_engine
-import datetime
+from spotipy import Spotify, SpotifyOAuth, SpotifyException
+from typing import Tuple, Any, List, Dict
 import os
-import pandas as pd
 import requests
-import spotipy
 
 load_dotenv(find_dotenv())
 
-spotify = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        scope=["playlist-read-private", "user-top-read"], # https://developer.spotify.com/documentation/web-api/concepts/scopes
-        client_id=os.getenv("SPOTIFY_CLIENT_ID"),
-        client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
-        redirect_uri="http://localhost",
-    )
-)
+def create_spotify_client() -> Spotify:
+    """
+    Create a Spotify client with OAuth2 authentication.
 
-#spotify = spotipy.Spotify(
-#    client_credentials_manager=SpotifyClientCredentials(
-#        client_id=os.getenv("SPOTIFY_CLIENT_ID"),
-#        client_secret=os.getenv("SPOTIFY_CLIENT_SECRET")
-#    )
-#)
+    This function creates a Spotify client with OAuth2 authentication using the client ID and client secret from the environment variables.
 
+    Returns:
+        Spotify: The Spotify client.
+    """
+    try:
+        spotify = Spotify(
+            auth_manager=SpotifyOAuth(
+                scope=["playlist-read-private", "user-top-read"], # https://developer.spotify.com/documentation/web-api/concepts/scopes
+                client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+                client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+                redirect_uri="http://localhost:8080",
+            )
+        )
+        return spotify
+    except SpotifyException as e:
+        logger.error(f"Could not create Spotify client. Error: {e}")
+        return None
+    
+def get_playlists(spotify: Spotify) -> Any:
+    """
+    Get a list of all playlists for the current user.
 
-# Get a list of all my playlists
-try:
-    playlists = spotify.current_user_playlists()
-    logger.success("Successfully got playlists from Spotify API")
-except:
-    logger.error("Could not get playlists from Spotify API")
+    This function uses the Spotify API to fetch all playlists for the current user.
 
-# Loop through the playlists and create a dictionary of playlist names and ids
-playlist_dict = {}
-for playlist in playlists["items"]:
-    if playlist["owner"]["id"] == "1244365996":
-        playlist_dict[playlist["name"]] = playlist["id"]
+    Args:
+        spotify (Spotify): The Spotify client.
 
-# Constants
-LIMIT = 100  # Maximum number of tracks to retrieve per API call
-
-# Initialize an empty list to hold all track dictionaries for all playlists
-tracks_list = []
+    Returns:
+        Any: The response from the Spotify API, containing the playlists.
+    """
+    try:
+        playlists = spotify.current_user_playlists()
+        logger.success("Successfully got playlists from Spotify API")
+        return playlists
+    except Exception as e:
+        logger.error(f"Could not get playlists from Spotify API. Error: {e}")
+        return None
 
 # Create a function to generate embed HTML for a given Spotify URI
 # https://developer.spotify.com/documentation/embeds/reference/oembed
-def generate_embed_html(uri):
+def generate_embed_html(uri: str) -> Tuple[str, str]:
+    """
+    Generate embed HTML for a given Spotify URI.
+
+    This function sends a GET request to the Spotify oEmbed API and returns the HTML and thumbnail URL from the response.
+
+    See https://developer.spotify.com/documentation/embeds/reference/oembed
+
+    Args:
+        uri (str): The Spotify URI to generate embed HTML for.
+
+    Returns:
+        Tuple[str, str]: A tuple containing the embed HTML and the thumbnail URL.
+    """
     response = requests.get(f'https://open.spotify.com/oembed/{uri}')
     return response.json()["html"], response.json()["thumbnail_url"]
 
-# Go through each playlist and get each playlist's ID
-logger.info("Getting tracks from each playlist")
-for playlist_name, playlist_id in playlist_dict.items():
-    # Initialize an offset
-    offset = 0
-    # Continuously fetch tracks until no more are available
-    while True:
-        # Get playlist tracks
-        try:
-            playlist_items = spotify.playlist_items(
-                playlist_id=playlist_id,
-                limit=LIMIT,
-                offset=offset
-            )
-        except Exception as e:
-            logger.error(f"Could not get tracks from {playlist_name} with offset {offset} because {e}")
-            break
-        # Check if there are no more tracks to fetch
-        if not playlist_items["items"]:
-            break
-        # Loop through each track in the playlist
-        logger.info(f"Getting tracks from {playlist_name} with offset {offset}")
-        for track in playlist_items["items"]:
-            # Initialize an empty dictionary for each track
-            track_dict = {}
-            # Fill the dictionary with the track's information
-            # https://developer.spotify.com/documentation/web-api/reference/get-playlists-tracks
-            track_dict["name"] = track["track"]["name"]
-            track_dict["artist"] = track["track"]["artists"][0]["name"]
-            artist_id = track["track"]["artists"][0].get("id")
-            # If the artist ID exists, get the artist's genre(s)
-            if artist_id:
-                track_dict["artist_id"] = artist_id
-                try:
-                    artist_info = spotify.artist(artist_id=artist_id)
-                    track_dict["artist_genre"] = artist_info.get("genres", [])
-                    images = artist_info.get("images", [{}]*3)
-                    track_dict["artist_images_large"] = images[0].get("url", None)
-                    track_dict["artist_images_medium"] = images[1].get("url", None)
-                    track_dict["artist_images_small"] = images[2].get("url", None)
-                except Exception as e:
-                    logger.error(f"Error fetching artist info for artist_id: {artist_id}. Error: {e}")
-                    track_dict["artist_genre"] = []
-                    track_dict["artist_images_large"] = None
-                    track_dict["artist_images_medium"] = None
-                    track_dict["artist_images_small"] = None
-            else:
-                track_dict["artist_id"] = None
-                track_dict["artist_genre"] = []
-                track_dict["artist_images_large"] = None
-                track_dict["artist_images_medium"] = None
-                track_dict["artist_images_small"] = None
-                logger.warning(f"No artist_id found for track {track_dict['name']}. Skipping artist info fetch.")
-            track_dict["explicit"] = track["track"]["explicit"]
-            track_dict["popularity"] = track["track"]["popularity"]
-            track_dict["duration_ms"] = track["track"]["duration_ms"]
-            track_dict["preview_url"] = track["track"]["preview_url"]
-            track_dict["album"] = track["track"]["album"]["name"]
-            track_dict["id"] = track["track"]["id"]
-            track_dict["added_at"] = track["added_at"]
-            track_dict["uri"] = track["track"]["uri"]
-            track_dict["embed_html"] = generate_embed_html(track_dict["uri"])[0]
-            track_dict["embed_thumbnail_url"] = generate_embed_html(track_dict["uri"])[1]
-            track_dict["playlist_name"] = playlist_name
-            logger.success(f"Successfully got track {track_dict['name']} info from {playlist_name}.")
-            # Append the track dictionary to the tracks list
-            tracks_list.append(track_dict)
-        # Increment the offset by the limit for the next iteration
-        offset += LIMIT
+def get_genre_for_artist(spotify: Any, artist_id: str) -> List[str]:
+    """
+    Fetch genres for a given artist ID.
 
-# Fetch genres for a given artist ID
-def get_genre_for_artist(spotify, artist_id):
+    This function uses the Spotify Web API to fetch the artist data for a given artist ID, and then returns the genres associated with that artist.
+
+    Args:
+        spotify (Any): The Spotify Web API client.
+        artist_id (str): The Spotify ID of the artist.
+
+    Returns:
+        List[str]: A list of genres associated with the artist.
+    """
     artist_data = spotify.artist(artist_id)
     genres = artist_data.get("genres", [])
     if not genres:
         logger.warning(f"No genres found for artist ID: {artist_id}, Artist Name: {artist_data.get('name')}")
     return genres
 
-# First, let's fetch the top artists and create a mapping from artist ID to genres
-def fetch_top_artists(spotify, time_ranges):
+
+def fetch_top_artists(spotify: Any, time_ranges: List[str]) -> List[Dict[str, Any]]:
+    """
+    Fetch the top artists for the current user for each time range.
+
+    This function uses the Spotify Web API to fetch the top artists for the current user for each time range, and then returns a list of dictionaries containing information about each artist.
+
+    Args:
+        spotify (Any): The Spotify Web API client.
+        time_ranges (List[str]): A list of time ranges to fetch the top artists for.
+
+    Returns:
+        List[Dict[str, Any]]: A list of dictionaries, each containing information about an artist.
+    """
     top_artists_list = []
     for time_range in time_ranges:
         artists = spotify.current_user_top_artists(limit=50, time_range=time_range)["items"]
@@ -147,13 +117,27 @@ def fetch_top_artists(spotify, time_ranges):
             artist_dict["images_medium"] = images[1].get("url")
             artist_dict["images_small"] = images[2].get("url")
             top_artists_list.append(artist_dict)
+            # Add the rank to the artist dictionary
+            for i, artist in enumerate(artists):
+                artist_dict = {...}
+                artist_dict["rank"] = i + 1
+                top_artists_list.append(artist_dict)
     return top_artists_list
 
-top_artists_list = fetch_top_artists(spotify, ["long_term", "medium_term", "short_term"])
-artist_genre_mapping = {artist['id']: artist['genres'] for artist in top_artists_list}
+def fetch_top_tracks(spotify: Any, time_ranges: List[str], artist_genre_mapping: Dict[str, List[str]]) -> List[Dict[str, Any]]:
+    """
+    Fetch the top tracks for the current user for each time range.
 
-# Now, fetch the top tracks
-def fetch_top_tracks(spotify, time_ranges, artist_genre_mapping):
+    This function uses the Spotify Web API to fetch the top tracks for the current user for each time range, and then returns a list of dictionaries containing information about each track.
+
+    Args:
+        spotify (Any): The Spotify Web API client.
+        time_ranges (List[str]): A list of time ranges to fetch the top tracks for.
+        artist_genre_mapping (Dict[str, List[str]]): A mapping from artist IDs to genres.
+
+    Returns:
+        List[Dict[str, Any]]: A list of dictionaries, each containing information about a track.
+    """
     top_tracks_list = []
     for time_range in time_ranges:
         tracks = spotify.current_user_top_tracks(limit=50, time_range=time_range)["items"]
@@ -181,25 +165,28 @@ def fetch_top_tracks(spotify, time_ranges, artist_genre_mapping):
             track_dict["embed_html"] = generate_embed_html(track["uri"])[0]
             track_dict["embed_thumbnail_url"] = generate_embed_html(track["uri"])[1]
             top_tracks_list.append(track_dict)
+            # Add the rank to the track dictionary
+            for i, track in enumerate(tracks):
+                track_dict = {...}
+                track_dict["rank"] = i + 1  # Add the rank to the track dictionary
+                top_tracks_list.append(track_dict)
+
     return top_tracks_list
 
-top_tracks_list = fetch_top_tracks(spotify, ["long_term", "medium_term", "short_term"], artist_genre_mapping)
+def fetch_top_data(spotify: Spotify) -> Tuple[List[Dict[str, str]], List[Dict[str, str]]]:
+    """
+    Fetch top artists and tracks for the current user.
 
-# Get the current datetime in ISO format
-current_timestamp = datetime.datetime.now().isoformat()
+    This function uses the Spotify API to fetch the top artists and tracks for the current user for each time range, and then returns a tuple containing a list of dictionaries with information about each artist and track.
 
-# Add the timestamp to your data
-for track_dict in tracks_list:
-    track_dict["timestamp"] = current_timestamp
+    Args:
+        spotify (Spotify): The Spotify client.
 
-for artist_dict in top_artists_list:
-    artist_dict["timestamp"] = current_timestamp
-
-for track_dict in top_tracks_list:
-    track_dict["timestamp"] = current_timestamp
-
-# Save the lists to the database
-db = create_engine(f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@127.0.0.1:5432/spotify")
-pd.DataFrame(tracks_list).to_sql("all_tracks", db, if_exists="append", index=False)
-pd.DataFrame(top_artists_list).to_sql("top_artists", db, if_exists="append", index=False)
-pd.DataFrame(top_tracks_list).to_sql("top_tracks", db, if_exists="append", index=False)
+    Returns:
+        Tuple[List[Dict[str, str]], List[Dict[str, str]]]: A tuple containing a list of dictionaries with information about each artist and track.
+    """
+    time_ranges = ["long_term", "medium_term", "short_term"]
+    top_artists_list = fetch_top_artists(spotify, time_ranges)
+    artist_genre_mapping = {artist['id']: artist['genres'] for artist in top_artists_list}
+    top_tracks_list = fetch_top_tracks(spotify, time_ranges, artist_genre_mapping)
+    return top_artists_list, top_tracks_list
